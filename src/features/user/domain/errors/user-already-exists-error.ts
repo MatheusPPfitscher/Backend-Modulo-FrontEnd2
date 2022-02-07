491e7c70570512a5d04cac403204d34b7c0b4de7
Matheus Petry Pfitscher <matheusppfitscher@outlook.com>
2022-02-06 21:01:45 -0300

    Correção bug usuário já existente

--- src/features/user/domain/errors/user-already-exists-error.ts
+++ src/features/user/domain/errors/user-already-exists-error.ts
@@ -2,7 +2,7 @@ import { DomainError } from "../../../../core/domain/errors/domain-error";
 
 export class UserAlreadyExistsError extends DomainError {
     constructor () {
-        super("Usuário já existe.", 409);
+        super("UserAlreadyExists", 409);
         this.name = "UserAlreadyExistsError";
     }
 }
