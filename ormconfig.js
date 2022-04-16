267cbdff84328502c932d49255af193b2eec56f5
Matheus Petry Pfitscher <matheusppfitscher@outlook.com>
2022-04-16 16:42:47 -0300

    Teste alteração configs

--- ormconfig.js
+++ ormconfig.js
@@ -1,34 +1,22 @@
-require("dotenv/config");
+require('dotenv/config');
 
-let config = {
+module.exports = {
     type: "postgres",
     url: process.env.DATABASE_URL,
-    entities: [process.env.ENTITIES_DIR],
-    migrations: [process.env.MIGRATIONS_DIR],
+    entities: [
+        process.env.ENTITIES_DIR
+    ],
+    migrations: [
+        process.env.MIGRATIONS_DIR
+    ],
     cli: {
-        entitiesDir: "src/core/infra/database/entities",
-        migrationsDir: "src/core/infra/database/migrations",
+        entitiesDir: 'src/core/infra/database/entities',
+        migrationsDir: 'src/core/infra/database/migrations'
     },
     synchronize: false,
     extra: {
-        ssl: {
-            rejectUnauthorized: false,
-        },
+        // ssl: {
+        //     rejectUnauthorized: false,
+        // },
     },
 };
-
-if (process.env.NODE_ENV === "test") {
-    config = {
-        type: "sqlite",
-        database: "./dbtest.sqlite",
-        entities: [process.env.ENTITIES_DIR],
-        migrations: ["tests/core/infra/database/migrations/**/*.ts"],
-        cli: {
-            entitiesDir: "src/core/infra/database/entities",
-            migrationsDir: "tests/core/infra/database/migrations",
-        },
-        synchronize: false,
-    };
-}
-
-module.exports = config;
